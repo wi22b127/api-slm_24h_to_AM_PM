82d08684e61920d6f412b26a8ae1c9c736a8366f
jevti <wi22b127@technikum-wien.at>
2023-09-11 18:07:48 +0200

    first commit

---
+++ src/main/java/at/technikum/slm_24h_to_am_pm/Slm24hToAmPmApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slm_24h_to_am_pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slm24hToAmPmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slm24hToAmPmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/slm_24h_to_am_pm/Slm24hToAmPmApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slm_24h_to_am_pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slm24hToAmPmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
